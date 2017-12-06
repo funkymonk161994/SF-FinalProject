ed85f502beb19521b230d211ac6dfef5908ef603
Mohitosh Mondal <mohit161994@gmail.com>
2017-12-06 18:20:55 -0500

    Added Write Custom messages and Read from NFC Tags

---
+++ app/src/main/java/sf/vimo/com/sf/Listener.java
@@ -0,0 +1,12 @@
+package sf.vimo.com.sf;
+
+/**
+ * Created by Mohitosh on 12/6/2017.
+ */
+
+public interface Listener {
+
+    void onDialogDisplayed();
+
+    void onDialogDismissed();
+}
--- app/src/main/java/sf/vimo/com/sf/Scanner.java
+++ app/src/main/java/sf/vimo/com/sf/Scanner.java
@@ -2,42 +2,147 @@ package sf.vimo.com.sf;
 
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
-public class Scanner extends Activity {
+public class Scanner extends Activity implements Listener {
 
-    private ProgressBar spinner;
+    public static final String TAG = Scanner.class.getSimpleName();
+
+    private EditText mEtMessage;
+    private Button mBtWrite;
+    private Button mBtRead;
+
+    private NFCWriteFragment mNfcWriteFragment;
+    private NFCReadFragment mNfcReadFragment;
+
+    private boolean isDialogDisplayed = false;
+    private boolean isWrite = false;
+
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanner);
-        Log.d("Scanner","onCreate");
+        setContentView(R.layout.activity_main);
 
-        Button sendData = findViewById(R.id.send);
-        sendData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try{
-                    Toast.makeText(getApplicationContext(),"Details Sent, looking for nearby contacts",Toast.LENGTH_SHORT).show();
-                    Intent save = new Intent(Scanner.this, Results.class);
-                    Log.d("Scanner","Found");
-                    startActivity(save);
-                }
-                catch (Exception e){
-                    e.printStackTrace();
+        initViews();
+        initNFC();
+    }
+
+    private void initViews() {
+
+        mEtMessage = (EditText) findViewById(R.id.et_message);
+        mBtWrite = (Button) findViewById(R.id.btn_write);
+        mBtRead = (Button) findViewById(R.id.btn_read);
+
+        mBtWrite.setOnClickListener(view -> showWriteFragment());
+        mBtRead.setOnClickListener(view -> showReadFragment());
+    }
+
+    private void initNFC(){
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    private void showWriteFragment() {
+
+        isWrite = true;
+
+        mNfcWriteFragment = (NFCWriteFragment) getFragmentManager().findFragmentByTag(NFCWriteFragment.TAG);
+
+        if (mNfcWriteFragment == null) {
+
+            mNfcWriteFragment = NFCWriteFragment.newInstance();
+        }
+        mNfcWriteFragment.show(getFragmentManager(),NFCWriteFragment.TAG);
+
+    }
+
+    private void showReadFragment() {
+
+        mNfcReadFragment = (NFCReadFragment) getFragmentManager().findFragmentByTag(NFCReadFragment.TAG);
+
+        if (mNfcReadFragment == null) {
+
+            mNfcReadFragment = NFCReadFragment.newInstance();
+        }
+        mNfcReadFragment.show(getFragmentManager(),NFCReadFragment.TAG);
+
+    }
+
+    @Override
+    public void onDialogDisplayed() {
+
+        isDialogDisplayed = true;
+    }
+
+    @Override
+    public void onDialogDismissed() {
+
+        isDialogDisplayed = false;
+        isWrite = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(mNfcAdapter!= null)
+            mNfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mNfcAdapter!= null)
+            mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        Log.d(TAG, "onNewIntent: "+intent.getAction());
+
+        if(tag != null) {
+            Toast.makeText(this, getString(R.string.message_tag_detected), Toast.LENGTH_SHORT).show();
+            Ndef ndef = Ndef.get(tag);
+
+            if (isDialogDisplayed) {
+
+                if (isWrite) {
+
+                    String messageToWrite = mEtMessage.getText().toString();
+                    mNfcWriteFragment = (NFCWriteFragment) getFragmentManager().findFragmentByTag(NFCWriteFragment.TAG);
+                    mNfcWriteFragment.onNfcDetected(ndef,messageToWrite);
+
+                } else {
+
+                    mNfcReadFragment = (NFCReadFragment)getFragmentManager().findFragmentByTag(NFCReadFragment.TAG);
+                    mNfcReadFragment.onNfcDetected(ndef);
                 }
             }
-        });
-
-
+        }
     }
 
 }
