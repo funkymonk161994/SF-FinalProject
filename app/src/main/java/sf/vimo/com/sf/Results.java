9767f706b5d29d71c7f8b0448fcb9dd2910917fd
Mohitosh Mondal <mohit161994@gmail.com>
2017-12-12 19:40:32 -0500

    Added few changes

--- app/src/main/java/sf/vimo/com/sf/Results.java
+++ app/src/main/java/sf/vimo/com/sf/Results.java
@@ -1,12 +1,16 @@
 package sf.vimo.com.sf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class Results extends AppCompatActivity {
 
@@ -18,6 +22,22 @@ public class Results extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         Log.d("Results","onCreate Started");
+        Intent in = getIntent();
+        TextView textView = findViewById(R.id.userName);
+        JSONObject jsonObject = new JSONObject();
+        try{
+            jsonObject = new JSONObject(in.getStringExtra("Read"));
+            Log.d("Read Values",jsonObject.toString());
+            textView.setText(jsonObject.getString("NAME"));
+            textView = findViewById(R.id.passWord);
+            textView.setText(jsonObject.getString("email"));
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
         addContact = findViewById(R.id.add);
         addContact.setOnClickListener(new View.OnClickListener() {
